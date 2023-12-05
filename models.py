e9066e4a25932084fd23765dbee3e3f4db4ddea9
avenegas91 <aven2601@yahoo.com>
2023-12-05 00:02:42 -0500

    Added/Edited content for Pt2 to Pt3

--- models.py
+++ models.py
@@ -5,10 +5,6 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-def connect_db(app):
-    db.app = app
-    db.init_app(app)
-
 DEFAULT_IMAGE_URL = "https://www.freeiconspng.com/uploads/icon-user-blue-symbol-people-person-generic--public-domain--21.png"
 
 # User Model
@@ -21,13 +17,15 @@ class User(db.Model):
     last_name = db.Column(db.Text, nullable = False)
     image_url = db.Column(db.Text, nullable = False, default = DEFAULT_IMAGE_URL)
 
+    posts = db.relationship("Post", backref="user", cascade="all, delete-orphan")
+
     @property
     def full_name(self):
         """Returns the full name of the user."""
 
         return f"{self.first_name} {self.last_name}"
     
-
+# Post Model
 class Post(db.Model):
     """Blog posts."""
 
@@ -48,3 +46,30 @@ class Post(db.Model):
 
         db.app = app
         db.init_app(app)
+
+class PostTag(db.Model):
+    """Tag on a post."""
+
+    __tablename__ = "post_tags"
+
+    post_id = db.Column(db.Integer, db.ForeignKey("posts.id"), primary_key=True)
+    tag_id = db.Column(db.Integer, db.ForeignKey("tags.id"), primary_key=True)
+
+class Tag(db.Model):
+    """Tag that can be added to a post."""
+
+    __tablename__ = "tags"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.Text, nullable=False, unique=True)
+
+    posts = db.relationship(
+        "Post",
+        secondary="posts_tags",
+        # cascade="all,delete",
+        backref="tags",
+    )
+
+def connect_db(app):
+    db.app = app
+    db.init_app(app)
